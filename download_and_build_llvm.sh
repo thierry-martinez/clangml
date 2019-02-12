c5d66361bc5814422f1b29d48d11665e08d4bb12
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-02-12 17:51:09 +0100

    support for LLVM 3.4

--- download_and_build_llvm.sh
+++ download_and_build_llvm.sh
@@ -5,22 +5,33 @@ if [ -z "$version" ]; then
     echo Missing version argument. >/dev/fd/2
     exit 1
 fi
+if [ "$version" "<" 3.4.1 ]; then
+    cfe=clang
+else
+    cfe=cfe
+fi
 if [ "$version" "<" 3.5 ]; then
     suffix=.tar.gz
 else
     suffix=.tar.xz
 fi
-case $version in
-    3.5*)
+if [ "$version" "<" 3.6 ]; then
+    if which -s gcc-4.9; then
+        CC=gcc-4.9
+        CXX+=g++-4.9
+    else
         CC=gcc-4.8
         CXX=g++-4.8
-        ;;
-esac
+    fi
+else
+    CC=gcc
+    CXX=g++
+fi
 wget http://releases.llvm.org/$version/llvm-$version.src$suffix
 tar -xf llvm-$version.src$suffix
-wget http://releases.llvm.org/$version/cfe-$version.src$suffix
-tar -xf cfe-$version.src$suffix
-mv cfe-$version.src llvm-$version.src/tools/clang
+wget http://releases.llvm.org/$version/$cfe-$version.src$suffix
+tar -xf $cfe-$version.src$suffix
+mv $cfe-$version.src llvm-$version.src/tools/clang
 mkdir llvm-$version.build
 pushd llvm-$version.build
     cmake ../llvm-$version.src -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
