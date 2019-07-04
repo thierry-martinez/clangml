1b0e23214ebd329ab792af51db813a99edd0766d
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-07-04 10:20:53 +0200

    Generate opam file by dune

--- ci-scripts/commit-snapshot-branch.sh
+++ ci-scripts/commit-snapshot-branch.sh
@@ -7,7 +7,7 @@ git reset --soft origin/snapshot
 grep -q AM_MAINTAINER_MODE configure.ac || \
 sed -i /AC_OUTPUT/iAM_MAINTAINER_MODE configure.ac
 ./bootstrap.sh
-git add -f configure.ac Makefile.in aclocal.m4 configure bootstrap
+git add -f configure.ac Makefile.in aclocal.m4 configure bootstrap clangml.opam
 if git commit -m "bootstrapped repository for commit $commit"; then
   git push origin snapshot
 fi
--- ci-scripts/opam-pin_and_install.sh
+++ ci-scripts/opam-pin_and_install.sh
@@ -8,6 +8,14 @@ URL="$1"
 cd ~/opam-repository
 git pull
 opam update
+
+# Pin override
+cd ~
+git clone https://gitlab.inria.fr/tmartine/override.git
+cd ~/override
+make override.opam
+opam pin add --yes --no-action file://~/override
+
 opam pin add --yes --no-action https://gitlab.inria.fr/tmartine/pattern.git
 opam pin add --yes --no-action "$URL"
 opam depext --yes --install clangml
