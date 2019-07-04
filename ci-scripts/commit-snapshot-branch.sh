0a4e3d9cfeefc4861988612ddf702ec134af5811
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-07-04 23:36:19 +0200

    Build clangml.opam for snapshot

--- ci-scripts/commit-snapshot-branch.sh
+++ ci-scripts/commit-snapshot-branch.sh
@@ -7,6 +7,7 @@ git reset --soft origin/snapshot
 grep -q AM_MAINTAINER_MODE configure.ac || \
 sed -i /AC_OUTPUT/iAM_MAINTAINER_MODE configure.ac
 ./bootstrap.sh
+dune build clangml.opam
 git add -f configure.ac Makefile.in aclocal.m4 configure bootstrap clangml.opam
 if git commit -m "bootstrapped repository for commit $commit"; then
   git push origin snapshot
