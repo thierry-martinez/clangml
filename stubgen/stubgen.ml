d5d86cd2b870a84c26a28d5fcb21f6c9d800a0bd
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-07-07 09:25:34 +0200

    Remove version equivalences

--- stubgen/stubgen.ml
+++ stubgen/stubgen.ml
@@ -1986,9 +1986,7 @@ let main cflags llvm_config prefix =
           | "5.0.0"
           | "5.0.1" -> "5.0.2"
           | "6.0.0" -> "6.0.1"
-          | "7.0.0"
-          | "7.1.0" -> "7.0.1"
-          | "8.0.1" -> "8.0.0"
+          | "7.0.0" -> "7.0.1"
           | _ -> llvm_version in
         String.split_on_char ' ' llvm_cflags @
         ["-I"; List.fold_left Filename.concat llvm_prefix
