255603ea121fd26b0112ed022a12d7006d337773
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-07-10 18:52:03 +0200

    Remove git clean

--- ci-scripts/commit-snapshot-branch.sh
+++ ci-scripts/commit-snapshot-branch.sh
@@ -4,7 +4,6 @@ commit=$(git rev-parse HEAD)
 git checkout snapshot
 git reset --hard master
 git reset --soft origin/snapshot
-git clean -d --force -x
 grep -q AM_MAINTAINER_MODE configure.ac || \
 sed -i /AC_OUTPUT/iAM_MAINTAINER_MODE configure.ac
 ./bootstrap.sh
