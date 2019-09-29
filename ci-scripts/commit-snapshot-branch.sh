38b8cd03f280978527cc79fa8edf3dfaf035af5a
Thierry Martinez <Thierry.Martinez@inria.fr>
2019-09-29 18:07:00 +0200

    Duplicate bootstrap.sh commands

--- ci-scripts/commit-snapshot-branch.sh
+++ ci-scripts/commit-snapshot-branch.sh
@@ -6,8 +6,10 @@ git reset --hard master
 git reset --soft origin/snapshot
 grep -q AM_MAINTAINER_MODE configure.ac || \
 sed -i /AC_OUTPUT/iAM_MAINTAINER_MODE configure.ac
-( cd src && ./bootstrap.sh )
-cp src/Makefile.in src/aclocal.m4 src/configure src/bootstrap src/clangml.opam .
+autoreconf
+rm -rf bootstrap
+git checkout origin/bootstrap bootstrap
+cp src/clangml.opam .
 git add -f configure.ac Makefile.in aclocal.m4 configure bootstrap clangml.opam
 if git commit -m "bootstrapped repository for commit $commit"; then
   git push origin snapshot
